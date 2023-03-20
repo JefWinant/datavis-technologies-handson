75e9088b5f15a1b17b144af762f0d9a18a6b1fc9
JefWinant <jef-winant@hotmail.com>
2023-03-20 14:45:31 +0100

    Exercise 8

---
+++ src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
@@ -0,0 +1,5 @@
+<script>
+const object = {name: 'Jef Winant', favthings: ["Gaming","Comics","Penguins"]}
+</script>
+
+My name is {object.name} and these are some of my favourite things: {object.favthings} .
