79d0832bef8b1fa8e58ad11f21b187ef1dc0d630
JefWinant <jef-winant@hotmail.com>
2023-03-20 15:12:11 +0100

    Exercise 10

---
+++ src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
@@ -0,0 +1,12 @@
+<script>
+import Exercise2 from '/home/jefwinant/datavis-technologies-handson/src/routes/02_exercises/01_web_technologies/exercise_02/Exercise.svelte'
+import Exercise3 from '/home/jefwinant/datavis-technologies-handson/src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte'
+</script>
+
+<div> 
+    <Exercise3 />
+</div>
+<div> 
+    <Exercise2 />
+</div>
+
